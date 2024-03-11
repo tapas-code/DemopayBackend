e5dd9980aea34e52aec0ff1f3a89e9e208fbdf04
kishlay <kishlay.kumar@spikewell.com>
2024-03-11 15:29:33 +0530

    Update Group Details

---
+++ Data/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace spayserver.Data.DTOs
+{
+    public class UpdateUserDTO
+    {
+        public string Username { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public decimal Amount { get; set; } = decimal.Zero;
+    }
+}
